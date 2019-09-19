4462277ab6f7841af78c32d9572f4f6c95d5b400
shuzang <844327696@qq.com>
2019-09-19 21:29:54 +0800

    use linkedlist implement stack

---
+++ Stack/linkedListStack/linkedListStack_test.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+var stack *Stack
+
+func init() {
+	stack = NewStack()
+}
+
+func BenchmarkPush(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		stack.Push(i)
+	}
+}
+
+func BenchmarkPop(b *testing.B) {
+	b.StopTimer()
+	for i := 0; i < b.N; i++ { //use b.N for looping
+		stack.Push("test")
+	}
+	b.StartTimer()
+	for i := 0; i < b.N; i++ { //use b.N for looping
+		fmt.Println(stack.Pop())
+	}
+}
+
+func TestPeek(t *testing.T) {
+	stack.Push("It's a test")
+	fmt.Println(stack.Peek())
+}
