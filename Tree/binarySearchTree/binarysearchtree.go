82f2767b755c0d9e9b0a870c3f6028cdeb462577
shuzang <lylw1996@qq.com>
2020-02-16 12:39:55 +0800

    refactor: 重新实现二叉搜索树基本功能

--- Tree/binarySearchTree/binarysearchtree.go
+++ Tree/binarySearchTree/binarysearchtree.go
@@ -2,187 +2,152 @@
 package binarysearchtree
 
 import (
-	"errors"
-	"fmt"
+	"container/list"
+	"reflect"
 )
 
-//Item 树节点的数据可以是任意类型
+//Item：树节点的数据可以是任意类型
 type Item interface{}
 
-//Node 二叉树节点结构
-type Node struct {
-	key   int   //以整型数据为例
-	value Item  //节点数据可以是任意类型
-	left  *Node //左儿子
-	right *Node //右儿子
+//TreeNode：结点结构
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
 }
 
-type basicFunction interface {
-	Insert(key int, value Item)     //以构造二叉搜索树的方式插入节点
-	PreOrderTraverse(f func(Item))  //前序遍历整棵树
-	InOrderTraverse(f func(Item))   //中序遍历整棵树
-	PostOrderTraverse(f func(Item)) //后序遍历整棵树
-	Remove(key int) error           //移除树中等于给定值的节点
-	Search(key int) (bool, error)   //如果树中存在与给定值相同的节点， 返回true,否则返回false
+//将传入数组转换成二叉树
+func arrayToBinTree(nums []Item) *TreeNode {
+	queue := list.New()
+	if reflect.TypeOf(nums[0]) == nil {
+		return nil
+	}
+	root := &TreeNode{nums[0].(int), nil, nil}
+	queue.PushBack(root)
+	for i := 1; queue.Len() != 0; {
+		root := queue.Remove(queue.Front()).(*TreeNode)
+		if i < len(nums) {
+			if reflect.TypeOf(nums[i]) != nil {
+				root.Left = &TreeNode{nums[i].(int), nil, nil}
+				queue.PushBack(root.Left)
+			}
+			i++
+		}
+		if i < len(nums) {
+			if reflect.TypeOf(nums[i]) != nil {
+				root.Right = &TreeNode{nums[i].(int), nil, nil}
+				queue.PushBack(root.Right)
+			}
+			i++
+		}
+	}
+	return root
 }
 
-//BinarySearchTree 二叉树
-type BinarySearchTree struct {
-	size int //树中节点总数
-	root *Node
+// 层次遍历
+func levelorderTraversal(root *TreeNode) []int {
+	result := []int{}
+	if root == nil {
+		return result
+	}
+	queue := list.New()
+	queue.PushBack(root)
+	for queue.Len() != 0 {
+		root = queue.Remove(queue.Front()).(*TreeNode)
+		result = append(result, root.Val)
+		if root.Left != nil {
+			queue.PushBack(root.Left)
+		}
+		if root.Right != nil {
+			queue.PushBack(root.Right)
+		}
+
+	}
+	return result
 }
 
-//Insert 以构造二叉树的方式插入节点
-func (bst *BinarySearchTree) Insert(key int, value Item) {
-	newNode := &Node{key, value, nil, nil}
-	if bst.root == nil {
-		bst.root = newNode
+/* // 二叉树搜索：递归解法
+func searchBST(root *TreeNode, val int) *TreeNode {
+    if root == nil {
+        return nil
+    }
+    if val < root.Val {
+        return searchBST(root.Left, val)
+    }else if val > root.Val {
+        return searchBST(root.Right, val)
+    }else{
+        return root
+    }
+} */
+
+//二叉树搜索：迭代解法
+func searchBST(root *TreeNode, val int) *TreeNode {
+	for root != nil {
+		if val < root.Val {
+			root = root.Left
+		} else if val > root.Val {
+			root = root.Right
+		} else {
+			return root
+		}
+	}
+	return nil
+}
+
+//搜索最小结点
+func findMin(root *TreeNode) *TreeNode {
+	if root != nil {
+		for root.Left != nil {
+			root = root.Left
+		}
+	}
+	return root
+}
+
+//搜索最大结点
+func findMax(root *TreeNode) *TreeNode {
+	if root != nil {
+		for root.Right != nil {
+			root = root.Right
+		}
+	}
+	return root
+}
+
+//二叉树插入
+func insertIntoBST(root *TreeNode, val int) *TreeNode {
+	if root == nil {
+		root = &TreeNode{val, nil, nil}
 	} else {
-		insertNode(bst.root, newNode)
+		if val > root.Val {
+			root.Right = insertIntoBST(root.Right, val)
+		} else if val < root.Val {
+			root.Left = insertIntoBST(root.Left, val)
+		}
 	}
-	bst.size++
+	return root
 }
 
-//寻找节点插入的合适位置
-func insertNode(node, newNode *Node) {
-	if newNode.key < node.key {
-		if node.left == nil {
-			node.left = newNode
-		} else {
-			insertNode(node.left, newNode)
-		}
+//二叉树删除
+func deleteNode(root *TreeNode, key int) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	if key < root.Val {
+		root.Left = deleteNode(root.Left, key)
+	} else if key > root.Val {
+		root.Right = deleteNode(root.Right, key)
 	} else {
-		if node.right == nil {
-			node.right = newNode
+		if root.Left != nil && root.Right != nil {
+			root.Val = findMin(root.Right).Val
+			root.Right = deleteNode(root.Right, root.Val)
 		} else {
-			insertNode(node.right, newNode)
+			if root.Left == nil {
+				root = root.Right
+			} else if root.Right == nil {
+				root = root.Left
+			}
 		}
 	}
+	return root
 }
-
-//PreOrderTraverse 前序遍历树中所有节点
-func (bst *BinarySearchTree) PreOrderTraverse(f func(Item)) {
-	preOrderTraverse(bst.root, f)
-}
-
-//前序遍历的递归函数
-func preOrderTraverse(node *Node, f func(Item)) {
-	if node != nil {
-		f(node.value)
-		preOrderTraverse(node.left, f)
-		preOrderTraverse(node.right, f)
-	}
-}
-
-//InOrderTraverse 中序遍历树中所有节点
-func (bst *BinarySearchTree) InOrderTraverse(f func(Item)) {
-	inOrderTraverse(bst.root, f)
-}
-
-//中序遍历的递归函数
-func inOrderTraverse(node *Node, f func(Item)) {
-	if node != nil {
-		inOrderTraverse(node.left, f)
-		f(node.value)
-		inOrderTraverse(node.right, f)
-	}
-}
-
-//PostOrderTraverse 后序遍历树中所有节点
-func (bst *BinarySearchTree) PostOrderTraverse(f func(Item)) {
-	postOrderTraverse(bst.root, f)
-}
-
-//后序遍历的递归函数
-func postOrderTraverse(node *Node, f func(Item)) {
-	if node != nil {
-		postOrderTraverse(node.left, f)
-		postOrderTraverse(node.right, f)
-		f(node.value)
-	}
-}
-
-//Remove 移除树中与给定值相同的节点
-func (bst *BinarySearchTree) Remove(key int) error {
-	_, err := remove(bst.root, key)
-	return err
-}
-
-//移除元素的递归函数
-func remove(node *Node, key int) (*Node, error) {
-	var err error
-	switch {
-	case node == nil:
-		return nil, errors.New("error: empty tree")
-	case key < node.key:
-		node.left, err = remove(node.left, key)
-		return node, err
-	case key > node.key:
-		node.right, err = remove(node.right, key)
-		return node, err
-	case node.left == nil && node.right == nil:
-		node = nil
-		return nil, nil
-	case node.left == nil:
-		node = node.right
-		return node, nil
-	case node.right == nil:
-		node = node.left
-		return node, nil
-	}
-	leftmostrightsubtree := node.right
-	for {
-		//寻找右子树中的最小值，位于右子树最左端
-		if leftmostrightsubtree != nil && leftmostrightsubtree.left != nil {
-			leftmostrightsubtree = leftmostrightsubtree.left
-		} else {
-			break
-		}
-	}
-	node.key, node.value = leftmostrightsubtree.key, leftmostrightsubtree.value
-	node.right, err = remove(node.right, node.key)
-	return node, err
-}
-
-//Search 搜索树中与给定值相同的节点，若存在，返回true
-func (bst *BinarySearchTree) Search(key int) (bool, error) {
-	return search(bst.root, key)
-}
-
-//搜索元素的递归函数
-func search(node *Node, key int) (bool, error) {
-	if node == nil {
-		return false, errors.New("errors: item not exist")
-	}
-	if key < node.key {
-		return search(node.left, key)
-	}
-	if key > node.key {
-		return search(node.right, key)
-	}
-	return true, nil
-}
-
-// String prints a visual representation of the tree
-func (bst *BinarySearchTree) String() {
-	fmt.Println("------------------------------------------------")
-	stringify(bst.root, 0)
-	fmt.Println("------------------------------------------------")
-}
-
-// internal recursive function to print a tree
-func stringify(n *Node, level int) {
-	if n != nil {
-		format := ""
-		for i := 0; i < level; i++ {
-			format += "       "
-		}
-		format += "---[ "
-		level++
-		stringify(n.left, level)
-		fmt.Printf(format+"%d\n", n.key)
-		stringify(n.right, level)
-	}
-}
-
-//https://flaviocopes.com/golang-data-structure-binary-search-tree/
--- Tree/binarySearchTree/binarysearchtree_test.go
+++ Tree/binarySearchTree/binarysearchtree_test.go
@@ -5,84 +5,20 @@ import (
 	"testing"
 )
 
-var bst BinarySearchTree
-
-func fillTree(bst *BinarySearchTree) {
-	bst.Insert(8, "8")
-	bst.Insert(4, "4")
-	bst.Insert(10, "10")
-	bst.Insert(2, "2")
-	bst.Insert(6, "6")
-	bst.Insert(1, "1")
-	bst.Insert(3, "3")
-	bst.Insert(5, "5")
-	bst.Insert(7, "7")
-	bst.Insert(9, "9")
+func TestSearchBST(t *testing.T) {
+	arr := []Item{4, 2, 7, 1, 3}
+	root := arrayToBinTree(arr)
+	fmt.Println(levelorderTraversal(searchBST(root, 2)))
 }
 
-func TestInsert(t *testing.T) {
-	fillTree(&bst)
-	bst.String()
-	bst.Insert(11, "11")
-	bst.String()
+func TestInsertIntoBST(t *testing.T) {
+	arr := []Item{4, 2, 7, 1, 3}
+	root := arrayToBinTree(arr)
+	fmt.Println(levelorderTraversal(insertIntoBST(root, 5)))
 }
 
-// isSameSlice returns true if the 2 slices are identical
-func isSameSlice(a, b []string) bool {
-	if a == nil && b == nil {
-		return true
-	}
-	if a == nil || b == nil {
-		return false
-	}
-	if len(a) != len(b) {
-		return false
-	}
-	for i := range a {
-		if a[i] != b[i] {
-			return false
-		}
-	}
-	return true
-}
-
-func TestInOrderTraverse(t *testing.T) {
-	var result []string
-	bst.InOrderTraverse(func(i Item) {
-		result = append(result, fmt.Sprintf("%s", i))
-	})
-	if !isSameSlice(result, []string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"}) {
-		t.Errorf("Traversal order incorrect, got %v", result)
-	}
-}
-
-func TestPreOrderTraverse(t *testing.T) {
-	var result []string
-	bst.PreOrderTraverse(func(i Item) {
-		result = append(result, fmt.Sprintf("%s", i))
-	})
-	if !isSameSlice(result, []string{"8", "4", "2", "1", "3", "6", "5", "7", "10", "9", "11"}) {
-		t.Errorf("Traversal order incorrect, got %v instead of %v", result, []string{"8", "4", "2", "1", "3", "6", "5", "7", "10", "9", "11"})
-	}
-}
-
-func TestPostOrderTraverse(t *testing.T) {
-	var result []string
-	bst.PostOrderTraverse(func(i Item) {
-		result = append(result, fmt.Sprintf("%s", i))
-	})
-	if !isSameSlice(result, []string{"1", "3", "2", "5", "7", "6", "4", "9", "11", "10", "8"}) {
-		t.Errorf("Traversal order incorrect, got %v instead of %v", result, []string{"1", "3", "2", "5", "7", "6", "4", "9", "11", "10", "8"})
-	}
-}
-
-func TestSearch(t *testing.T) {
-	bst.Search(1)
-}
-
-func TestRemove(t *testing.T) {
-	bst.Remove(1)
-	if fmt.Sprintf("%s", *bst.Min()) != "2" {
-		t.Errorf("min should be 2")
-	}
+func TestDeleteNode(t *testing.T) {
+	arr := []Item{5, 3, 6, 2, 4, nil, 7}
+	root := arrayToBinTree(arr)
+	fmt.Println(levelorderTraversal(deleteNode(root, 3)))
 }
